7a20a9dbd065fe16cab1f938aa892991e33917a2
Charles Hollenbeck <charles@hollenbeck.pw>
2013-09-28 18:57:26 -0400

    Update gitpush.rb

--- ruby/gitpush.rb
+++ ruby/gitpush.rb
@@ -5,15 +5,12 @@ remotes = Array.new
 branches = Array.new
 tags = false
 mult_branch = false
-verbose = false
 
 ARGV.each do|a|
   if a == "-t" or a == "--tags" and tags == false
     tags = true
   elsif a == "-b" or a == "--branches" and mult_branch == false
     mult_branch = true
-  elsif a == "-v" or a == "--verbose" and verbose == false
-    verbose == true
   elsif mult_branch
     branches << a
   else
@@ -22,19 +19,16 @@ ARGV.each do|a|
 end
 
 branches << "master" unless mult_branch
-for i in 0..branches.count
-  for j in 0..remotes.count
+for j in 0..remotes.count
+  for i in 0..branches.count
     cmd = "git push #{remotes[j]} #{branches[i]}"
     cmd += " --tags" if tags
-    if verbose
-      puts %x(cmd)
+    puts "Pushing branch \"#{branches[i]}\" to remote \"#{remotes[j]}\""
+
+    if system(cmd)
+      puts "Push successful"
     else
-      puts "Pushing branch \"#{branches[i]}\" to remote \"#{remotes[j]}\""
-      if system(cmd)
-        puts "Push successful"
-      else
-        puts "Push failed! (Try using the -v tag for a more verbose output)"
-      end
+      puts "Push failed! (Try using the -v tag for a more verbose output)"
     end
   end
 end
